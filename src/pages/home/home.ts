591056776ed28b86561637b4ba1bc41f68d9076f
yuta-ushijima <register@yuta-u.com>
2018-10-19 10:02:30 +0900

    Messege Board of CRUD

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,87 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, AlertController } from 'ionic-angular';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  /*プロパティの定義*/
+  post: {
+    userName: string,
+    message: string,
+    createdDate: any
+    } = {
+    userName: 'Yuta Ushizima',
+    message: 'テストメッセージ',
+    createdDate: '10分前'
+    };
+    message: string;
 
-  constructor(public navCtrl: NavController) {
+    posts: { userName: string, message: string, createdDate: any }[]
+      = [
+      {
+        userName: 'ちー',
+        message: 'にゃーん',
+        createdDate: '15分前'
+      },
+      {
+        userName: 'はく',
+        message: 'にゃん',
+        createdDate: '30分前'
+      }
+    ];
+
+    constructor(public navCtrl: NavController,
+                public alertCtrl: AlertController) {
 
   }
+
+  addPost() {
+  /*入力されたメッセージを使って、投稿データを作成*/
+  this.post = {
+    userName: 'Mariko Ushijima',
+    messsage: this.message,
+    createdDate: '数秒前'
+  };
+  /*配列postsにpostを追加*/
+  this.posts.push(this.post);
+  /*入力フィールドを空にする*/
+  this.message = '';
+  }
+
+  presentPrompt(index: number) {
+  let alert = this.alertCtrl.create({
+    title: 'メッセージ編集',
+    inputs: [
+      {
+        name: 'message',
+        placeholder: 'メッセージ'
+      }
+     ],
+     buttons: [
+       {
+         text: 'キャンセル',
+         role: 'cancel',
+         hanler: () => {
+           console.log('キャンセルされました');
+         }
+       },
+       {
+         text: '更新',
+         handler: data => {
+         console.log(data);
+         this.posts[index].message = data.message;
+         }
+       }
+      ]
+    });
+    alert.present();
+   }
+
+   deletePost(index: number) {
+   /*splice()を使って、配列postsからindex番目のオブジェクトを削除*/
+     this.posts.splice(index, 1);
+   }
 
 }
