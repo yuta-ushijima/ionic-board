cc043f2ca682aed0c684091670f94bac11a23075
Yuta Ushijima <yuta_ushizima@YutanoMacBook-puro.local>
2018-10-21 22:17:59 +0900

    FireBaseによる投稿の永続化

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,16 @@ import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
+
 // Import Firebase Config
 import { environment } from "../environments/environment";
+
 //module of firebase
 import { AngularFireModule } from "@angular/fire";
 import { AngularFireAuthModule } from "@angular/fire/auth";
+import { AngularFirestoreModule } from "@angular/fire/firestore";
 
+// Import Custome Component
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
 import { LoginPage } from "../pages/login/login";
@@ -25,7 +29,8 @@ import { SignupPage } from "../pages/signup/signup";
     BrowserModule,
     IonicModule.forRoot(MyApp),
     AngularFireModule.initializeApp(environment.firebase),
-    AngularFireAuthModule
+    AngularFireAuthModule,
+    AngularFirestoreModule
   ],
   bootstrap: [IonicApp],
   entryComponents: [
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,56 +1,88 @@
 import { Component } from '@angular/core';
-import { NavController, AlertController } from 'ionic-angular';
+import { NavController, AlertController, ToastController } from 'ionic-angular';
+import { AngularFirestore, AngularFirestoreCollection } from "@angular/fire/firestore";
+import { AngularFireAuth } from "@angular/fire/auth";
+import * as firebase from 'firebase';
+
+// 定義したPostのInterfaceをインポート
+import { Post } from "../../app/models/post";
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+
   /*プロパティの定義*/
-  post: {
-    userName: string,
-    message: string,
-    createdDate: any
-    } = {
-    userName: 'Yuta Ushizima',
-    message: 'テストメッセージ',
-    createdDate: '10分前'
-    };
-    message: string;
+  message: string;
+  // 型宣言に定義したPostインターフェイスを使うことで、
+  // Postインターフェイスが持っているメンバーや型を
+  // プロパティに強制適用させることができる
+  post: Post;
+  //[]を使って、インターフェイスが持っている型を配列として指定できる。
+  posts: Post[];
 
-    posts: { userName: string, message: string, createdDate: any }[]
-      = [
-      {
-        userName: 'ちー',
-        message: 'にゃーん',
-        createdDate: '15分前'
-      },
-      {
-        userName: 'はく',
-        message: 'にゃん',
-        createdDate: '30分前'
-      }
-    ];
+  postsCollection: AngularFirestoreCollection<Post>;
 
-    constructor(public navCtrl: NavController,
-                public alertCtrl: AlertController) {
 
+
+  constructor(public navCtrl: NavController,
+              private toastCtrl: ToastController,
+              private alertCtrl: AlertController,
+              private afStore: AngularFirestore,
+              private afAuth: AngularFireAuth) {
+
+  }
+
+  ionViewWillEnter() {
+    this.getPosts();
   }
 
   addPost() {
   /*入力されたメッセージを使って、投稿データを作成*/
   this.post = {
-    userName: 'Mariko Ushijima',
+    id: "",
+    userName: this.afAuth.auth.currentUser.displayName,
     message: this.message,
-    createdDate: '数秒前'
+    created: firebase.firestore.FieldValue.serverTimestamp()
   };
-  /*配列postsにpostを追加*/
-  this.posts.push(this.post);
-  /*入力フィールドを空にする*/
-  this.message = '';
+
+  // Firebaseにデータを追加
+  this.afStore.collection('posts').add(this.post)
+    //投稿に成功した場合
+    .then( (docRef) =>{
+      this.postsCollection.doc(docRef.id).update({
+        id: docRef.id
+      });
+      /*入力フィールドを空にする*/
+      this.message = '';
+    })
+    //投稿に失敗したらToastでエラーを表示
+    .catch((error) => {
+      this.toastCtrl.create({
+        message: error,
+        duration: 6000
+      }).present();
+    });
+
   }
 
-  presentPrompt(index: number) {
+  getPosts() {
+    //投稿というコレクションの参照を取得
+    this.postsCollection = this.afStore.collection(
+      'posts',
+      ref => ref.orderBy('created', "desc")
+    );
+
+    //データの変更をここで受け取る
+    this.postsCollection.valueChanges()
+      .subscribe( data => {
+        this.posts = data;
+      });
+  }
+
+
+  presentPrompt(post: Post) {
   let alert = this.alertCtrl.create({
     title: 'メッセージ編集',
     inputs: [
@@ -70,8 +102,8 @@ export class HomePage {
        {
          text: '更新',
          handler: data => {
-         console.log(data);
-         this.posts[index].message = data.message;
+         //投稿を更新するメソッドの呼び出し
+           this.updatePost(post, data.message);
          }
        }
       ]
@@ -79,9 +111,38 @@ export class HomePage {
     alert.present();
    }
 
-   deletePost(index: number) {
-   /*splice()を使って、配列postsからindex番目のオブジェクトを削除*/
-     this.posts.splice(index, 1);
-   }
+  //メッセージの更新
+  updatePost(post: Post, message: string) {
+    //入力されたメッセージで投稿を更新
+    this.postsCollection.doc(post.id).update({
+      message: message
+    }).then(() => {
+      this.toastCtrl.create({
+        message: '投稿が更新されました',
+        duration: 3000
+      }).present();
+    }).catch((error) => {
+      this.toastCtrl.create({
+        message: error,
+        duration: 6000
+      }).present()
+    })
+  }
+
+  //投稿の削除
+  deletePost(post: Post) {
+    this.postsCollection.doc(post.id).delete()
+      .then(() => {
+        this.toastCtrl.create({
+          message: '投稿が削除されました',
+          duration: 4000
+        }).present();
+      }).catch((error) => {
+        this.toastCtrl.create({
+          message: error,
+          duration: 6000
+        }).present();
+    })
+  }
 
 }
